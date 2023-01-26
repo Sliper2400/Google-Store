39ddf010b1345739e0c94d2c44a806382b459437
Sliper2400 <playkulikov@gmail.com>
2023-01-26 12:54:37 +0200

    add something

--- The_Eiffel_tower.js
+++ The_Eiffel_tower.js
@@ -1,2 +1,36 @@
-const title_words = document.getElementById("preview_title").getElementsByTagName("span");
-alert(1);
+const title_h1_arr = document.getElementById("main-title").getElementsByTagName("h1");
+const title_p_arr = document.getElementById("main-title-p-cont").getElementsByTagName("p");
+const title_input_arr = document.getElementById("main-title-button-cont").getElementsByTagName("p");
+ 
+
+setTimeout(() => {
+    let anim_time = 0;
+    let time;
+    for (let a =0; a < title_h1_arr.length; a++, time = 110 * (a + 1))
+        setTimeout(() => {
+            title_h1_arr[a].classList.add("main-title-word-vis");
+        }, time);
+    anim_time += time;
+    time = 110 + anim_time;
+    for (let a =0; a < title_p_arr.length; a++, time = 110 * (a + 1) + anim_time)
+        setTimeout(() => {
+            title_p_arr[a].classList.add("main-title-word-vis");
+        }, time);
+    anim_time =+ time;
+    time= 110 + anim_time;
+
+    setTimeout(() => {
+        document.getElementById("main-title-button").classList.add("main-title-input-vis");
+    }, anim_time);
+    for (let a =title_input_arr.length - 1; a >= 0; a--, time = 110 * (title_input_arr.length - a) + anim_time)
+        setTimeout(() => {
+            title_input_arr[a].classList.add("main-title-word-vis");
+        }, time);
+
+}, 300);
+
+
+
+
+
+
